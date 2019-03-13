ca2f32f1f9391bb9403cf7d97cd65e6ca0172c6d
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-13 20:57:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect { |name| badge_maker(name) }
+end
+
+def assign_rooms(array)
+  array.collect { |room| puts "Hello, #{name}! You'll be assigned to room #{room}!"}
+end
